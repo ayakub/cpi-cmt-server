5e15f27ca477538ccdbe673bb2a68f652cdb90fb
ayakub <ayakub1086@gmail.com>
2023-01-23 02:20:23 +0600

    make teachers api

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors');
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 // middleware
@@ -18,9 +18,10 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const noticeCollection = client.db("cpiCmt").collection("notice");
+        const teacherCollection = client.db("cpiCmt").collection("teacher");
 
 
-
+        //addded notice
         app.post('/notice', async (req, res) => {
             const notice = req.body;
             // console.log(notice)
@@ -34,11 +35,30 @@ async function run() {
             const result = await noticeCollection.find(query).toArray();
             res.send(result);
         })
-        app.get('/noticeAll', async (req, res) => {
-            const query = {};
-            const result = await noticeCollection.find(query).toArray();
+
+        app.get('/noticeAll/:id', async (req, res) => {
+            const id = req.params.i;
+            const query = { _id: ObjectId(id) };
+            const result = await noticeCollection.findOne(query)
             res.send(result);
         })
+
+
+        //added teacher
+
+        app.post('/addTeacher', async (req, res) => {
+            const teacher = req.body;
+            console.log(teacher)
+            const result = await teacherCollection.insertOne(teacher);
+            res.send(result)
+        })
+        app.get('/addTeacher/:designation', async (req, res) => {
+            const designation = req.params.designation;
+            const query = { designation }
+            const result = await teacherCollection.find(query).toArray();
+            res.send(result)
+        })
+
 
 
 
